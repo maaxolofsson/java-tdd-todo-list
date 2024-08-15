609dc2031532bcad8216f68d228e9a6d30c41c77
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-15 10:36:29 +0200

    Implemented code for adding a task in todo list

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,17 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class TodoList {
 
+    private ArrayList<Task> tasks;
+
+    public TodoList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public boolean add(Task task) {
+        this.tasks.add(task);
+        return true;
+    }
 }
