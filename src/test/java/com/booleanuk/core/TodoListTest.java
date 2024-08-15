7aafb85765a93a3c31178e6cc560249338a2bd70
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-15 10:48:15 +0200

    Added test for checking so that all tasks can be fetched

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,6 +3,8 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 class TodoListTest {
 
     public TodoListTest() {
@@ -22,10 +24,27 @@ class TodoListTest {
     }
 
     @Test
-    void testAddTaskToTodoListAlreadyExists() {
+    public void testAddTaskToTodoListAlreadyExists() {
         TodoList tl = new TodoList();
         Assertions.assertTrue(tl.add(new Task("Do the dishes")));
         Assertions.assertFalse(tl.add(new Task("Do the dishes")));
     }
+
+    @Test
+    public void testgetAllTasks() {
+        TodoList tl = new TodoList();
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        tasks.add(new Task("Do the dishes"));
+        tasks.add(new Task("Do the laundry"));
+        tasks.add(new Task("Vacuum"));
+
+        for (Task t : tasks) {
+            tl.add(t);
+        }
+
+        Assertions.assertEquals(tasks, tl.getAll());
+    }
+
 
 }
